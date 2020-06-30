3f6b05d86c11a0dfc1a27b0f830b928d2bc337da
Ahmed Bule <ahmedbule01@gmail.com>
2020-06-30 11:26:48 -0500

    added servers and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Chicken');
+INSERT INTO burgers (name) VALUES ('Beef');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey', false);
+INSERT INTO burgers (name, devoured) VALUES ('Philly', false);
+INSERT INTO burgers (name, devoured) VALUES ('Fish', false);
+INSERT INTO burgers (name) VALUES ('Ham');
