08daba124c566158dc03b67fb50696342e79fa45
Ahmed Bule <ahmedbule01@gmail.com>
2020-07-05 17:39:01 -0500

    fixed the functionality of create route

---
+++ public/js/burger.js
@@ -0,0 +1,63 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devoured").on("click", function(event) {
+      var id = $(this).attr("id");
+      var burgerData = $(this).data("devour");
+        console.log("Update",id);
+      var data = {
+        devoured: burgerData
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: data
+      }).then(
+        function() {
+          console.log("Burger has been updated", data);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burger").val().trim(),
+       devoured: $("[name=devour]:checked").val().trim()
+       
+      };
+      console.log(newBurger);
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Placed an Order");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).attr("id");
+    console.log("delete",id)
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted a Burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +19,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
+app.use(express.static("public"));
 
 app.use(routes);
 
